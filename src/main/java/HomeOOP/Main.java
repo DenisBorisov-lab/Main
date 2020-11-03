08e62e60a0849459cb52e650addd91cb78862b95
Denis <mai.borisov.denis@gmail.com>
2020-11-03 10:43:55 +0300

    some text

--- src/main/java/HomeOOP/Main.java
+++ src/main/java/HomeOOP/Main.java
@@ -1,2 +1,15 @@
-package HomeOOP;public class Main {
+package HomeOOP;
+
+public class Main {
+    public static void main(String[] args) {
+        Phone message = new Phone();
+        System.out.println(message.model);
+        System.out.println(message.number);
+        System.out.println(message.weight);
+        System.out.println("------------------------------------");
+        System.out.println(message.receiveCall("John"));
+        System.out.println("Номер: " + message.getNumber());
+        System.out.println("------------------------------------");
+        System.out.println(message.sendMessage("84956667589"));
+    }
 }
--- src/main/java/HomeOOP/Phone.java
+++ src/main/java/HomeOOP/Phone.java
@@ -1,2 +1,27 @@
-package HomeOOP;public class Phone {
+package HomeOOP;
+
+public class Phone {
+    String number;
+    int weight;
+    String model;
+    Phone(){
+        number = "88005553535";
+        weight = 12;
+        model = "Iphone-12";
+    }
+    public String receiveCall(String name){
+        String mes = "Звонит " + name;
+        return mes;
+    }
+    public String receiveCall(long number){
+        return "Звонит " + number;
+    }
+    public String getNumber(){
+        String num = "89254568976";
+        return num;
+    }
+    public String sendMessage(String number){
+        String res = "Отправить сообщение: " + number;
+        return res;
+    }
 }
